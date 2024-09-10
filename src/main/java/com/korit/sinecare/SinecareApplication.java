4d95424f59a0b0fe16e6e45e4c6baa8fef7efd7d
SeongJoon95 <gnsxmtkfkd@naver.com>
2024-09-10 10:31:16 +0900

    2024-09-10 13:30

---
+++ src/main/java/com/korit/sinecare/SinecareApplication.java
@@ -0,0 +1,13 @@
+package com.korit.sinecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinecareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korit/sinecare/SinecareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.sinecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
